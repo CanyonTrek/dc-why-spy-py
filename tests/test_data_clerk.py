ba904625f9613e0349311d2d9f8423365e1949ff
Donald Cameron <donald.cameron@qa.com>
2025-06-24 16:16:39 +0100

    Very minor Changes as Py does not need Interfaces and does not use Mockito

--- tests/test_data_clerk.py
+++ tests/test_data_clerk.py
@@ -1,5 +1,6 @@
 import unittest
 from app.data_clerk import DataClerk, FileLog
+from unittest.mock import Mock
 
 class DataClerkTest(unittest.TestCase):
     def test_process_data(self):
@@ -13,6 +14,20 @@ class DataClerkTest(unittest.TestCase):
         # Assert
         # No assert, just checking that it runs (like the original Java test)
         # Output must be inspected manually or via logging
+
+
+    def test_files_are_logged_before_8pm_mocked(self):
+        # Arrange
+        mock_file_log = Mock()
+        cut = DataClerk(mock_file_log)
+
+        # Act
+        cut.process_data()
+
+        # Assert
+        mock_file_log.clear_the_log.assert_called_once()
+
+
 
 if __name__ == '__main__':
     unittest.main()
