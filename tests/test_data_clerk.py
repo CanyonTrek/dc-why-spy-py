c0e5bec27051977ccf684e6b7eb16c7b149838e6
Donald Cameron <donald.cameron@qa.com>
2025-06-24 16:34:17 +0100

    Added test for wrongly logged after 8pm

--- tests/test_data_clerk.py
+++ tests/test_data_clerk.py
@@ -28,6 +28,23 @@ class DataClerkTest(unittest.TestCase):
         mock_file_log.clear_the_log.assert_called_once()
 
 
+    def test_files_are_wrongly_logged_after_8pm_mocked(self):
+        # This test will fail if the real system time is before 8pm
+        # because DataClerk uses current time in its logic.
+        # In Git tag v1.4, DataClerk was updated to accept the current time as an argument.
+
+        # Arrange
+        mock_file_log = Mock()
+        cut = DataClerk(mock_file_log)
+
+        # Act
+        # WARNING: This uses real system time and fails before 8pm
+        cut.process_data()
+
+        # Assert
+        mock_file_log.clear_the_log.assert_not_called()
+
+
 
 if __name__ == '__main__':
     unittest.main()
